21bf9a0a02d2bb2c3f4fa206d635a5d7264281f0
DarynHolmes <DarynHolmes@gmail.com>
2015-09-13 21:16:18 +0100

    Added examples

---
+++ scripts/unobtrusive.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    // document.getElementById('date').onchange = validateDate;
+    alert("HI");
+};
+
+function showElement(id) {
+    document.getElementById(id).style.visibility = 'visible';
+}
+
+function hideElement(id) {
+    document.getElementById(id).style.visibility = 'hidden';
+}
